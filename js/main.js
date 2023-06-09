29355a6f2a8f529c464e05e62a3979fe2309345d
MariaRipley <roblesb.maria@gmail.com>
2023-06-09 10:24:11 +0200

    Add practices from lesson 1.16

--- js/main.js
+++ js/main.js
@@ -19,24 +19,6 @@ const GITHUB_USER = '<IreneTaPa>';
 const SERVER_URL = `https://dev.adalab.es/api/kittens/${GITHUB_USER}`;
 
 //Objetos con cada gatito
-/*const kittenData_1 = {
-  image: 'https://dev.adalab.es/gato-siames.webp',
-  name: 'Anastacio',
-  desc: 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
-  race: 'Siamés',
-};
-const kittenData_2 = {
-  image: 'https://dev.adalab.es/sphynx-gato.webp',
-  name: 'Fiona',
-  desc: 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
-  race: 'Sphynx',
-};
-const kittenData_3 = {
-  image: 'https://dev.adalab.es/maine-coon-cat.webp',
-  name: 'Cielo',
-  desc: ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
-  race: 'Maine Coon',
-};*/
 
 let kittenDataList = [];
 const kittenListStored = JSON.parse(localStorage.getItem('kittensList'));
@@ -62,21 +44,33 @@ if (kittenListStored) {
 
 //Funciones
 function renderKitten(kittenData) {
-  const kitten = `<li class="card">
-    <article>
-      <img
-        class="card_img"
-        src=${kittenData.image}
-        alt="gatito"
-      />
-      <h3 class="card_title">${kittenData.name}</h3>
-      <h3 class="card_race">${kittenData.race}</h3>
-      <p class="card_description">
-      ${kittenData.desc}
-      </p>
-    </article>
-    </li>`;
-  return kitten;
+    const liElement = document.createElement('li');
+    liElement.classList.add('card');
+    const articleElement = document.createElement('article');
+    liElement.appendChild(articleElement);
+    const imgElement = document.createElement('img');
+    imgElement.classList.add('card_img');
+    imgElement.src = kittenData.image;
+    const h3Title = document.createElement('h3');
+    h3Title.classList.add('card_title');
+    articleElement.appendChild(h3Title);
+
+    return liElement;
+//   const kitten = `<li class="card">
+//     <article>
+//       <img
+//         class="card_img"
+//         src=${kittenData.image}
+//         alt="gatito"
+//       />
+//       <h3 class="card_title">${kittenData.name}</h3>
+//       <h3 class="card_race">${kittenData.race}</h3>
+//       <p class="card_description">
+//       ${kittenData.desc}
+//       </p>
+//     </article>
+//     </li>`;
+//   return kitten;
 }
 
 function renderKittenList(kittenDataList) {
